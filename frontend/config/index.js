5bad06c516ee49ca55c08a0408db4b9e6d88e531
sahityaaryan <sahityaaryansingh@gmail.com>
2025-02-07 17:37:25 +0530

    workiing setup - 2

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,5 +1,12 @@
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://tpc.nitp.ac.in"
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://tpc.nitp.ac.in'
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
+
+
+
+// export const API_URL =
+// process.env.NEXT_PUBLIC_API_URL || "https://tpc.nitp.ac.in"
+// export const NEXT_URL =
+// process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://tpc.nitp.ac.in'
