016394cb5c1c0ac2200e038b3c5ab9ae3af1c25b
sahityaaryan <sahityaaryansingh@gmail.com>
2025-02-08 18:41:32 +0530

    Revert "workiing setup - 2"
    This reverts commit 5bad06c516ee49ca55c08a0408db4b9e6d88e531.

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,12 +1,5 @@
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
+  process.env.NEXT_PUBLIC_API_URL || "https://tpc.nitp.ac.in"
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000'
-
-
-
-// export const API_URL =
-// process.env.NEXT_PUBLIC_API_URL || "https://tpc.nitp.ac.in"
-// export const NEXT_URL =
-// process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://tpc.nitp.ac.in'
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://tpc.nitp.ac.in'
