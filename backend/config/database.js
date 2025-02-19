b3850d2af0381334d3da84f0e7aaffe5a5bba7dd
sahityaaryan <sahityaaryansingh@gmail.com>
2025-02-19 15:45:23 +0530

    backend production setup

--- backend/config/database.js
+++ backend/config/database.js
@@ -26,15 +26,10 @@ module.exports = ({ env }) => {
       connection: {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
-
         port: env.int('DATABASE_PORT', 5432),
-
         database: env('DATABASE_NAME', 'tnp-test'),
-
         user: env('DATABASE_USERNAME', 'postgres'),
-
         password: env('DATABASE_PASSWORD', '2006001'),
-
         schema: env('DATABASE_SCHEMA', 'public'),
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
