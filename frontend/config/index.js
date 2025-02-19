62b6d202e6abad6858a0014ed86568ed77c0120b
sahityaaryan <sahityaaryansingh@gmail.com>
2025-02-19 13:18:18 +0530

    prod setup

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://tpc.nitp.ac.in"
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://tpc.nitp.ac.in'
+  process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://tpc.nitp.ac.in '
